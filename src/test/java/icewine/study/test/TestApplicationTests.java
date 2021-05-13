e62e85d645e1d5e298bf4fe493417bcdb5a2e95a
ice <3376129179@qq.com>
2021-05-13 16:42:36 +0800

    frist commit

---
+++ src/test/java/icewine/study/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package icewine.study.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
